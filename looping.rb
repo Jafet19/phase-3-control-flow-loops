c8b2d751f9a8d909b0c5060656ed651114fafbce
Jafet Reyes Jr <jafet_19@Jafets-MacBook-Pro.local>
2022-10-11 16:14:38 -0500

    commit tree 5f96e43d01cbad8a1a10c3c1b168ecdb987494bf

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
   # your code here
+  counter = 10
+  until counter == 0
+    puts counter
+    counter -= 1 
+  end
+    puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +23,16 @@ end
 
 def fizzbuzz_printer
   # your code here
+   (1..100).each do |counter|
+    puts  fizzbuzz(counter)
+end
 end
 
 def reverse_string(str)
   # your code here
+  reverse = ''
+  str.length.times do |i|
+    reverse = str[i] + reverse
+  end
+  reverse
 end
